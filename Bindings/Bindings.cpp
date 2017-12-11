f1546332cb2dcac957161efb356053886835fa14
mahnen <mahnen@phys.ethz.ch>
2017-12-11 15:44:47 +0100

    more pybindings for mctracer, getting there

--- Bindings/Bindings.cpp
+++ Bindings/Bindings.cpp
@@ -11,9 +11,28 @@
 #include "Core/RayForPropagation.h"
 #include "Core/Photon.h"
 #include "Core/Frame.h"
+#include "Core/Color.h"
+#include "Core/Function/LimitsFunction.h"
+#include "Core/Function/Func1DFunction.h"
 #include "Scenery/Primitive/Triangle.h"
-
+#include "Scenery/Primitive/Annulus.h"
+#include "Scenery/Primitive/BiConvexLens.h"
+#include "Scenery/Primitive/BiConvexLensHexBound.h"
+#include "Scenery/Primitive/Cylinder.h"
+#include "Scenery/Primitive/Disc.h"
+#include "Scenery/Primitive/EllipticalCapWithHexagonalBound.h"
+#include "Scenery/Primitive/HexPlane.h"
+#include "Scenery/Primitive/Plane.h"
+#include "Scenery/Primitive/PlaneDualSphericalBound.h"
+#include "Scenery/Primitive/RectangularBox.h"
+#include "Scenery/Primitive/Sphere.h"
+#include "Scenery/Primitive/SphereCapWithCylinderBound.h"
+#include "Scenery/Primitive/SphereCapWithHexagonalBound.h"
+#include "Scenery/Primitive/SphereCapWithRectangularBound.h"
 //---------------------------------
+
+using Function::Func1D;
+using Function::Limits;
 
 PYBIND11_MODULE(mctPy, module) {
     module.doc() = "mctracer Python bindings";  // optional module docstring
@@ -54,14 +73,47 @@ PYBIND11_MODULE(mctPy, module) {
         .def(pybind11::init<double, double, double>(),
             pybind11::arg("Phi"), pybind11::arg("The"), pybind11::arg("Psi"))
         .def(pybind11::init<const Vec3, const double>(),
-            pybind11::arg("new_rot_axis"), pybind11::arg("new_rot_angle"))
+            pybind11::arg("rot_axis"), pybind11::arg("rot_angle"))
         .def("__repr__", &Rot3::str);
+
+    pybind11::class_<Color>(module, "Color")
+        .def(pybind11::init<const int, const int, const int>(),
+            pybind11::arg("_r"), pybind11::arg("_g"), pybind11::arg("_b"))
+        .def("__repr__", &Color::str);
 
     pybind11::class_<SurfaceEntity, Frame>(module, "SurfaceEntity")
         .def(pybind11::init<const std::string, const Vec3, const Rot3>(),
-            pybind11::arg("new_name"), pybind11::arg("new_pos"),
-            pybind11::arg("new_rot"))
+            pybind11::arg("name"), pybind11::arg("pos"), pybind11::arg("rot"))
+        .def("set_outer_color", &SurfaceEntity::set_outer_color,
+            pybind11::arg("color"))
+        .def("set_inner_color", &SurfaceEntity::set_inner_color,
+            pybind11::arg("color"))/*
+        .def("set_outer_reflection", &SurfaceEntity::set_outer_reflection,
+            pybind11::arg("refl"))
+        .def("set_inner_reflection", &SurfaceEntity::set_inner_reflection,
+            pybind11::arg("refl"))
+        .def("set_outer_refraction", &SurfaceEntity::set_outer_refraction,
+            pybind11::arg("refrac"))
+        .def("set_inner_refraction", &SurfaceEntity::set_inner_refraction,
+            pybind11::arg("refrac"))
+        .def("set_outer_absorption", &SurfaceEntity::set_outer_absorption,
+            pybind11::arg("absorp"))
+        .def("set_inner_absorption", &SurfaceEntity::set_inner_absorption,
+            pybind11::arg("absorp"))*/
+        .def("set_allowed_frames_to_propagate_to", &SurfaceEntity::set_allowed_frames_to_propagate_to,
+            pybind11::arg("frame"))
+        .def("take_boundary_layer_properties_from", &SurfaceEntity::take_boundary_layer_properties_from,
+            pybind11::arg("proto"))
+        .def("take_boundary_layer_properties_but_inside_out_from", 
+            &SurfaceEntity::take_boundary_layer_properties_but_inside_out_from,
+            pybind11::arg("proto"))
+        .def("boundary_layer_is_transparent", 
+            &SurfaceEntity::boundary_layer_is_transparent)
+        .def("has_restrictions_on_frames_to_propagate_to", 
+            &SurfaceEntity::has_restrictions_on_frames_to_propagate_to)
         .def("__repr__", &SurfaceEntity::str);
+
+// ---------------- the primitives ------------------------
 
     pybind11::class_<Triangle, SurfaceEntity>(module, "Triangle")
         .def(pybind11::init<>())
@@ -73,4 +125,104 @@ PYBIND11_MODULE(mctPy, module) {
             pybind11::arg("nomal"), pybind11::arg("a"),
             pybind11::arg("b"), pybind11::arg("c"))
         .def("__repr__", &Triangle::str);
+
+    pybind11::class_<Annulus, SurfaceEntity>(module, "Annulus")
+        .def(pybind11::init<>())
+        .def("set_outer_inner_radius", &Annulus::set_outer_inner_radius,
+            pybind11::arg("outer_radius"), pybind11::arg("inner_radius"))
+        .def("__repr__", &Annulus::str);
+
+    pybind11::class_<BiConvexLens, SurfaceEntity>(module, "BiConvexLens")
+        .def(pybind11::init<>())
+        .def("set_curvature_radius_and_aperture_radius",
+            &BiConvexLens::set_curvature_radius_and_aperture_radius,
+            pybind11::arg("curvature_radius"), pybind11::arg("aperture_radius"))
+        .def("__repr__", &BiConvexLens::str);
+
+    pybind11::class_<BiConvexLensHexBound, SurfaceEntity>(module, "BiConvexLensHexBound")
+        .def(pybind11::init<>())
+        .def("set_curvature_radius_and_outer_hex_radius",
+            &BiConvexLensHexBound::set_curvature_radius_and_outer_hex_radius,
+            pybind11::arg("curvature_radius"), pybind11::arg("outer_aperture_radius"))
+        .def("__repr__", &BiConvexLensHexBound::str);
+
+    pybind11::class_<Cylinder, SurfaceEntity>(module, "Cylinder")
+        .def(pybind11::init<const std::string, const Vec3, const Rot3>(),
+            pybind11::arg("name"), pybind11::arg("pos"), pybind11::arg("rot"))
+        .def("set_cylinder", &Cylinder::set_cylinder,
+            pybind11::arg("radius"), pybind11::arg("start_pos"), pybind11::arg("end_pos"))
+        .def("set_radius_and_length", &Cylinder::set_radius_and_length,
+            pybind11::arg("radius"), pybind11::arg("length"))
+        .def("__repr__", &Cylinder::str);
+
+    pybind11::class_<Disc, SurfaceEntity>(module, "Disc")
+        .def(pybind11::init<>())
+        .def("set_radius", &Disc::set_radius, pybind11::arg("radius"))
+        .def("__repr__", &Disc::str);
+
+    pybind11::class_<EllipticalCapWithHexagonalBound, SurfaceEntity>(module, "EllipticalCapWithHexagonalBound")
+        .def(pybind11::init<>())
+        .def("set_focalLength_radiiRatio_hexAngel_hexRadius",
+            &EllipticalCapWithHexagonalBound::set_focalLength_radiiRatio_hexAngel_hexRadius,
+            pybind11::arg("focal_length"), pybind11::arg("ratio_x_to_y_curvature_radius"),
+            pybind11::arg("hex_bound_rotation"), pybind11::arg("outer_hex_radius"))
+        .def("__repr__", &EllipticalCapWithHexagonalBound::str);
+
+    pybind11::class_<HexPlane, SurfaceEntity>(module, "HexPlane")
+        .def(pybind11::init<>())
+        .def("set_outer_hex_radius", &HexPlane::set_outer_hex_radius,
+            pybind11::arg("outer_hex_radius"))
+        .def("__repr__", &HexPlane::str);
+
+    pybind11::class_<Plane, SurfaceEntity>(module, "Plane")
+        .def(pybind11::init<const std::string, const Vec3, const Rot3>(),
+            pybind11::arg("name"), pybind11::arg("pos"), pybind11::arg("rot"))
+        .def("set_x_y_width", &Plane::set_x_y_width,
+            pybind11::arg("x_width"), pybind11::arg("y_width"))
+        .def("__repr__", &Plane::str);
+
+    pybind11::class_<PlaneDualSphericalBound, SurfaceEntity>(module, "PlaneDualSphericalBound")
+        .def(pybind11::init<>())
+        .def("set_x_hight_and_y_width",
+            &PlaneDualSphericalBound::set_x_hight_and_y_width,
+            pybind11::arg("x_width"), pybind11::arg("y_width"))
+        .def("__repr__", &PlaneDualSphericalBound::str);
+
+    pybind11::class_<RectangularBox, SurfaceEntity>(module, "RectangularBox")
+        .def(pybind11::init<>())
+        .def("set_xyz_width",
+            &RectangularBox::set_xyz_width,
+            pybind11::arg("x"), pybind11::arg("y"), pybind11::arg("z"))
+        .def("__repr__", &RectangularBox::str);
+
+    pybind11::class_<Sphere, SurfaceEntity>(module, "Sphere")
+        .def(pybind11::init<const std::string, const Vec3, const Rot3>(),
+            pybind11::arg("name"), pybind11::arg("pos"), pybind11::arg("rot"))
+        .def("set_radius", &Sphere::set_radius, pybind11::arg("radius"))
+        .def("__repr__", &Sphere::str);
+
+    pybind11::class_<SphereCapWithCylinderBound, SurfaceEntity>(module, "SphereCapWithCylinderBound")
+        .def(pybind11::init<>())
+        .def("set_curvature_radius_and_outer_radius", 
+            &SphereCapWithCylinderBound::set_curvature_radius_and_outer_radius,
+            pybind11::arg("curvature_radius"), pybind11::arg("cap_radius"))
+        .def("__repr__", &SphereCapWithCylinderBound::str);
+
+    pybind11::class_<SphereCapWithHexagonalBound, SurfaceEntity>(module, "SphereCapWithHexagonalBound")
+        .def(pybind11::init<>())
+        .def("set_curvature_radius_and_outer_hex_radius", 
+            &SphereCapWithHexagonalBound::set_curvature_radius_and_outer_hex_radius,
+            pybind11::arg("curvature_radius"), pybind11::arg("outer_hex_radius"))
+        .def("__repr__", &SphereCapWithHexagonalBound::str);
+
+    pybind11::class_<SphereCapWithRectangularBound, SurfaceEntity>(module, "SphereCapWithRectangularBound")
+        .def(pybind11::init<>())
+        .def("set_curvature_radius_and_x_y_width", 
+            &SphereCapWithRectangularBound::set_curvature_radius_and_x_y_width,
+            pybind11::arg("_curvature_radius"), pybind11::arg("x_width"), pybind11::arg("y_width"))
+        .def("__repr__", &SphereCapWithRectangularBound::str);
+
+// --------------------- primitives end ------------------------
+
 }
+
